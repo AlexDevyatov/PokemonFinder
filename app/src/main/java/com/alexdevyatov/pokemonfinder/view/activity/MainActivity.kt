76d9437846406324804e5d657be70b145e151e45
alexdevyatov <alexeidevyatov94@gmail.com>
2020-02-15 21:36:29 +0400

    делаю навигацию

--- app/src/main/java/com/alexdevyatov/pokemonfinder/view/activity/MainActivity.kt
+++ app/src/main/java/com/alexdevyatov/pokemonfinder/view/activity/MainActivity.kt
@@ -2,14 +2,17 @@ package com.alexdevyatov.pokemonfinder.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import com.alexdevyatov.pokemonfinder.R
 import com.alexdevyatov.pokemonfinder.view.fragment.MainFragment
+import com.alexdevyatov.pokemonfinder.view.fragment.MainFragmentDirections
+import com.alexdevyatov.pokemonfinder.view.fragment.SearchPokemonFragment
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       // supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, MainFragment()).commit()
     }
 }
--- app/src/main/java/com/alexdevyatov/pokemonfinder/view/fragment/MainFragment.kt
+++ app/src/main/java/com/alexdevyatov/pokemonfinder/view/fragment/MainFragment.kt
@@ -6,17 +6,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 import com.alexdevyatov.pokemonfinder.R
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
+
+    private var btnSearch : Button? = null;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        btnSearch = view.findViewById(R.id.btnSearch)
+        btnSearch!!.setOnClickListener {
+            val action = MainFragmentDirections.actionMainFragmentToSearchPokemonFragment()
+            it.findNavController().navigate(action)
+        }
+        return view
     }
 
 
